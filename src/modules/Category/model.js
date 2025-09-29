b088b520a475b47ae5b0bcccb9b7b372cbd182ca
waseemAnsari11011 <waseemahm11011@gmail.com>
2025-09-29 17:40:37 +0530

    abc

--- src/modules/Category/model.js
+++ src/modules/Category/model.js
@@ -1,29 +1,34 @@
 // Import mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define the Category Schema
-const categorySchema = new mongoose.Schema({
+const categorySchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     images: {
-        type: [String],  // Array of strings to store image URLs or paths
-        validate: [arrayLimit, '{PATH} exceeds the limit of 10'] // Optional: Limit the number of images
+      type: [String], // Array of strings to store image URLs or paths
+      validate: [arrayLimit, "{PATH} exceeds the limit of 10"], // Optional: Limit the number of images
     },
-
-}, {
-    timestamps: true // Automatically add createdAt and updatedAt timestamps
-});
+    addresses: {
+      type: [String], // Array of strings to store addresses
+    },
+  },
+  {
+    timestamps: true, // Automatically add createdAt and updatedAt timestamps
+  }
+);
 
 // Custom validator to limit array size
 function arrayLimit(val) {
-    return val.length <= 10;
+  return val.length <= 10;
 }
 
 // Create the Category Model
-const Category = mongoose.model('Category', categorySchema);
+const Category = mongoose.model("Category", categorySchema);
 
 // Export the Category Model
 module.exports = Category;
